bef68b0445a60158c61fb734956f1f5902061f80
Justin Blagden <jbemails2@gmail.com>
2013-02-10 16:01:35 -0600

    Added updated schema to the commit

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130210204642) do
+ActiveRecord::Schema.define(:version => 20130210213049) do
 
   create_table "nodes", :force => true do |t|
     t.string   "mac_address",                     :null => false
@@ -44,6 +44,8 @@ ActiveRecord::Schema.define(:version => 20130210204642) do
     t.datetime "created_at",                                :null => false
     t.datetime "updated_at",                                :null => false
     t.boolean  "admin",                  :default => false
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
