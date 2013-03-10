62621ca40643748f2c03c177e089698701180efa
Justin Blagden <jbemails2@gmail.com>
2013-03-10 17:39:05 -0500

    Direct the user to their own page after an update

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -10,12 +10,12 @@ class RegistrationsController < Devise::RegistrationsController
   
   def after_sign_up_path_for(resource)
     #devise/registrations#new
-    '/users/sign_up'
+    user_path
   end
   
   def after_inactive_sign_up_path_for(resource)
     #devise/registrations#new
-    '/users/sign_up'
+    user_path
   end
 
 end
