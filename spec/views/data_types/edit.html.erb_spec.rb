b3b38e39e61f299cc8970d236b0950d888305830
Brian Turchyn <brian@dj-bri-t.net>
2013-03-17 12:02:24 -0500

    Updated data types view specs to match new data formats underneath.

--- spec/views/data_types/edit.html.erb_spec.rb
+++ spec/views/data_types/edit.html.erb_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe "data_types/edit" do
   before(:each) do
     @data_type = assign(:data_type, stub_model(DataType,
-      :name => "MyString",
-      :unit => "MyString"
+      :longhand_unit => "MyString",
+      :shorthand_unit => "MyString"
     ))
   end
 
@@ -13,8 +13,8 @@ describe "data_types/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => data_types_path(@data_type), :method => "post" do
-      assert_select "input#data_type_name", :name => "data_type[name]"
-      assert_select "input#data_type_unit", :name => "data_type[unit]"
+      assert_select "input#data_type_longhand_unit", :name => "data_type[longhand_unit]"
+      assert_select "input#data_type_shorthand_unit", :name => "data_type[shorthand_unit]"
     end
   end
 end
