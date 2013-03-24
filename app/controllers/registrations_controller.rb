ab321fced95614d0e6f2552ef6688f6c2a81cc28
Tyler <tseredynski@gmail.com>
2013-03-24 17:27:39 -0500

    Edited user Update and Edit definitions sth: Allow admin status to be edited by admins in user profile pages. Do not require current password to be entered when editing profile pages. Updated tests for user profile pages.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -12,7 +12,32 @@ class RegistrationsController < Devise::RegistrationsController
     
     respond_to do |format|
       format.html # edit.html.erb
-      format.json { render json: @users }
+      format.json { render json: @user }
+    end
+  end
+  
+  def update
+    @user = User.find_by_id(params[:id])
+    if params[:user][:password].blank?
+       params[:user].delete("password")
+       params[:user].delete("password_confirmation")
+    end
+    self.resource = resource_class.to_adapter.get!(@user.to_key)
+    prev_unconfirmed_email = resource.unconfirmed_email if resource.respond_to?(:unconfirmed_email)
+        
+    if resource.update_attributes(resource_params)
+      if is_navigational_format?
+        flash_key = update_needs_confirmation?(resource, prev_unconfirmed_email) ?
+          :update_needs_confirmation : :updated
+        set_flash_message :notice, flash_key
+      end
+      if @user == current_user      
+        sign_in resource_name, resource, :bypass => true
+      end
+      respond_with resource, :location => after_update_path_for(resource)
+    else
+      clean_up_passwords resource
+      respond_with resource
     end
   end
 
@@ -26,6 +51,10 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
     #devise/registrations#new
     '/users/sign_up'
+  end
+  
+  def after_update_path_for(resource)
+    dashboard_path
   end
   
   def authorized_user?
--- features/users/user_profile_tests.feature
+++ features/users/user_profile_tests.feature
@@ -1,14 +1,23 @@
-Feature: allow non-admin users to edit their own profile
+Feature: allow non-admin users to edit their own profile and admins to edit anyone's profile
 
 	As a non-admin user
 	I want to edit my profile
 	So that I can update my profile
 	
+	As an admin user
+	I want to edit a user's profile
+	So that I can update that user's profile
+	
 	Scenario: a non-admin user edits their profile
 		Given I am registered as non-admin user "nonadmin1@example.com" with password "password"
 		When I logon as "nonadmin1@example.com" with password "password"
 		And I go to my profile edit page
-		Then I should see "Edit User"
+		And I enter "password_new" in "Password"
+		And I enter "password_new" in "Password confirmation"
+		And I click the button "Update"
+		And I logout
+		And I logon as "nonadmin1@example.com" with password "password_new"
+		Then I should be on the dashboard
 
     Scenario: prevent non-admin users from editing non-admin users' profiles
 		Given I am registered as non-admin user "nonadmin1@example.com" with password "password"
