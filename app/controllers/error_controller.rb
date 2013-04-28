a335209f50c44de4ccae7737a801326f575c0dcb
Brian Turchyn <brian@dj-bri-t.net>
2013-04-28 15:00:20 -0500

    Added custom error pages.

---
+++ app/controllers/error_controller.rb
@@ -0,0 +1,26 @@
+class ErrorController < ApplicationController
+  skip_before_filter :authenticate_user!
+
+  layout 'logged_out'
+
+  def error_403
+    handle_error 403
+  end
+
+  def error_404
+    handle_error 404
+  end
+
+  def error_500
+    handle_error 500
+  end
+
+  private
+
+  def handle_error(err)
+    respond_to do |format|
+      format.html { render :status => err }
+      format.all  { render :nothing => true, :status => err }
+    end
+  end
+end
