fd9f2c3b19ebe85a05fe2e5edec967330324a1c1
Brian Turchyn <brian@dj-bri-t.net>
2014-03-29 00:11:48 -0500

    Added Security Events link to admin#index

--- spec/views/admin/index.html.erb_spec.rb
+++ spec/views/admin/index.html.erb_spec.rb
@@ -11,5 +11,6 @@ describe "admin/index.html.erb" do
     assert_select "li", :text => "Sensors".to_s, :count => 1
     assert_select "li", :text => "User Accounts".to_s, :count => 1
     assert_select "li", :text => "User Signup".to_s, :count => 1
+    assert_select "li", :text => "Security Events".to_s, :count => 1
   end
 end
